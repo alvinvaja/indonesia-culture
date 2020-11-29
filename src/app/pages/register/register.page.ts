2b965f4b96867cc56976681a9a3b449485903a09
MiguelGouw <miguel.gouw@gmail.com>
2020-11-29 15:19:30 +0700

    regis lowercase

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,36 +1,40 @@
-import { Router } from '@angular/router';
-import { AuthenticateService } from './../../services/authentication.service';
+import { Router } from "@angular/router";
+import { AuthenticateService } from "./../../services/authentication.service";
 // register.page.ts
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { LoadingController, NavController } from '@ionic/angular';
-import { UsersService } from 'src/app/services/users.service';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from "@angular/forms";
+import { LoadingController, NavController } from "@ionic/angular";
+import { UsersService } from "src/app/services/users.service";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
+  selector: "app-register",
+  templateUrl: "./register.page.html",
+  styleUrls: ["./register.page.scss"],
 })
 export class RegisterPage implements OnInit {
   validationForm: FormGroup;
-  errorMessage = '';
-  successMessage = '';
+  errorMessage = "";
+  successMessage = "";
 
   validationMessages = {
     email: [
-      { type: 'required', message: 'Email is required.' },
-      { type: 'pattern', message: 'Enter a valid email.' }
+      { type: "required", message: "Email is required." },
+      { type: "pattern", message: "Enter a valid email." },
     ],
     password: [
-      { type: 'required', message: 'Password is required.' },
-      { type: 'minlength', message: 'Password must be at least 5 characters long.' }
+      { type: "required", message: "Password is required." },
+      {
+        type: "minlength",
+        message: "Password must be at least 5 characters long.",
+      },
     ],
-    name : [
-      { type: 'required', message: 'Name is required.' }
-    ],
-    age : [
-      {type: 'required', message: 'Age is required'}
-    ]
+    name: [{ type: "required", message: "Name is required." }],
+    age: [{ type: "required", message: "Age is required" }],
   };
 
   constructor(
@@ -40,44 +44,45 @@ export class RegisterPage implements OnInit {
     private loadingCtrl: LoadingController,
     private router: Router,
     private userService: UsersService
-
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.validationForm = this.formBuilder.group({
-      email: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
-      ])),
-      password: new FormControl('', Validators.compose([
-        Validators.minLength(5),
-        Validators.required
-      ])),
-      name: new FormControl('', Validators.compose([
-        Validators.required
-      ])),
-      age: new FormControl('', Validators.compose([
-        Validators.required
-      ]))
+      email: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$"),
+        ])
+      ),
+      password: new FormControl(
+        "",
+        Validators.compose([Validators.minLength(5), Validators.required])
+      ),
+      name: new FormControl("", Validators.compose([Validators.required])),
+      age: new FormControl("", Validators.compose([Validators.required])),
     });
   }
 
   tryRegister(value: any) {
-    this.authService.registerUser(value)
-      .then(res => {
+    value.email = value.email.toLowerCase();
+    this.authService.registerUser(value).then(
+      (res) => {
         console.log(res);
         this.createuser();
-        this.errorMessage = '';
-        this.successMessage = 'Your account has been created. Please log in.';
-      }, err => {
+        this.errorMessage = "";
+        this.successMessage = "Your account has been created. Please log in.";
+      },
+      (err) => {
         console.log(err);
         this.errorMessage = err.message;
-        this.successMessage = '';
-      });
+        this.successMessage = "";
+      }
+    );
   }
 
   goLoginPage() {
-    this.navCtrl.navigateBack('/login');
+    this.navCtrl.navigateBack("/login");
   }
 
   async createuser() {
@@ -90,6 +95,6 @@ export class RegisterPage implements OnInit {
 
     this.userService.registerToFireStore(email, name, age, contribution);
 
-    this.router.navigateByUrl('/login');
+    this.router.navigateByUrl("/login");
   }
 }
--- src/app/pages/tabs/leaderboard/leaderboard.page.ts
+++ src/app/pages/tabs/leaderboard/leaderboard.page.ts
@@ -1,34 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/models/users.model';
-import { UsersService } from 'src/app/services/users.service';
+import { Component, OnInit } from "@angular/core";
+import { User } from "src/app/models/users.model";
+import { UsersService } from "src/app/services/users.service";
 
 @Component({
-  selector: 'app-leaderboard',
-  templateUrl: './leaderboard.page.html',
-  styleUrls: ['./leaderboard.page.scss'],
+  selector: "app-leaderboard",
+  templateUrl: "./leaderboard.page.html",
+  styleUrls: ["./leaderboard.page.scss"],
 })
 export class LeaderboardPage implements OnInit {
   public users: User[];
-  public currentEmail = '';
-  constructor(
-    private userService: UsersService
-  ) { }
+  public currentEmail = "";
+  constructor(private userService: UsersService) {}
 
   ngOnInit() {
-    this.currentEmail = localStorage.getItem('email') === null ? '' : localStorage.getItem('email');
+    this.currentEmail =
+      localStorage.getItem("email") === null
+        ? ""
+        : localStorage.getItem("email");
     this.users = [];
-    this.userService.getAllUsers().subscribe(res => {
-      this.users = res.filter(data => {
-        return data.email !== 'admin@mailnator.com';
+    this.userService.getAllUsers().subscribe((res) => {
+      this.users = res.filter((data) => {
+        return data.email !== "admin@mailnator.com";
       });
-      this.users.sort( (a, b) => {
+      this.users.sort((a, b) => {
         if (a.contribution > b.contribution) {
           return -1;
         } else {
           return 1;
         }
       });
+      console.log(this.users);
     });
   }
-
 }
