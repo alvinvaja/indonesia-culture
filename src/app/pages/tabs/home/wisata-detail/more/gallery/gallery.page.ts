20371321b3c410804451f253f819d7d11c81a704
Alvin Valenciano <alvinvaja@gmail.com>
2020-12-08 00:51:04 +0700

    must logged in before upload wisata photo

--- src/app/pages/tabs/home/wisata-detail/more/gallery/gallery.page.ts
+++ src/app/pages/tabs/home/wisata-detail/more/gallery/gallery.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { Wisata } from 'src/app/models/wisata.model';
 import { WisataPhoto } from 'src/app/models/wisataPhoto.model';
 import { WisataService } from 'src/app/services/wisata.service';
@@ -17,7 +18,8 @@ export class GalleryPage implements OnInit {
   constructor(
     private wisataService: WisataService,
     private activatedRoute: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private alertCtrl: AlertController
   ) { }
 
   ngOnInit() {
@@ -42,7 +44,36 @@ export class GalleryPage implements OnInit {
     });
   }
 
+  isLoggedIn() {
+    return localStorage.getItem('name') === null ? false : true;
+  }
+
   goToPhoto() {
+    if (!this.isLoggedIn()) {
+      this.presentAlert();
+      return;
+    }
+
     this.router.navigateByUrl('/tabs/home/' + this.wisataId + '/photo');
+  }
+
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      message: 'You need to login to continue this action.',
+      buttons: [
+        {
+          text: 'Ok'
+        },
+        {
+          text: 'Go to Login',
+          cssClass: 'secondary',
+          handler: () => {
+            this.router.navigateByUrl('/login');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 }
